9205621375d6b13dd78bc2e7a0da9a244b230d2c
Supardi <supardi9512@gmail.com>
2019-08-06 00:39:02 +0700

    pemanggilan-api-get

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,19 +1,45 @@
 import React, {Component, Fragment} from 'react';
 import './BlogPost.css';
 import Post from '../../component/Post/Post';
+import axios from 'axios';
 
 class BlogPost extends Component {
-    componentDidMount() {
-        
+    state = {
+        post: []
     }
+    componentDidMount() {
+        // Memanggil API menggunakan cara default dari JSX
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        // .then(response => response.json())
+        // .then(json => {
+        //     this.setState({
+        //         post: json
+        //     })
+        // })
+
+        // Memanggil menggunakan AXIOS
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((result) => {
+            console.log(result.data);
+            this.setState({
+                post: result.data
+            })
+        })
+    }
+
     render() {
         return(
             <Fragment>
                 <p class="section-title">Blog Post</p>
-                <Post title="title" desc="desc" />
+                {
+                    // looping data dari API
+                    this.state.post.map(post => {
+                        return <Post key={post.id} title={post.title} desc={post.body} />
+                    })
+                }
             </Fragment>
         )
     }
 }
-   aaaa
+ 
 export default BlogPost;
