2a2554e5a69451da16370bf4dcac672aa88f0ef0
REKOR iMac 01 <rekorimac01@REKORs-iMac.local>
2019-08-07 09:52:56 +0700

    put-api

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -11,7 +11,8 @@ class BlogPost extends Component {
             title: '',
             body: '',
             userId: 1
-        }
+        },
+        isUpdate: false
     } 
 
     // fungsi Untuk memanggil data get 
@@ -29,23 +30,59 @@ class BlogPost extends Component {
         axios.post('http://localhost:3004/posts', this.state.formBlogPost).then((res) => {
             console.log(res);
             this.getPostAPI();
+            this.setState({
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                },
+            })
         }, (err) => {
             console.log('error: ', err); // menampilkan error jika error
         })
     }
 
+    // method untuk mengupdate data
+    putDataToAPI = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost).then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            this.setState({
+                isUpdate: false,
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                },
+            })
+        })
+    }
     // menghapus data dengan axios
     handleRemove = (data) => {
         axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
-            this.getPostAPI()
+            this.getPostAPI();
         })
+    }
+
+    // menampilkan data di form ketika ingin diedit
+    handleUpdate = (data) => {
+        console.log(data);
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
+        
     }
 
     handleFormChange = (event) => {
         // console.log('form change', event.target)
         let formBlogPostNew = {...this.state.formBlogPost};
         let timestamp = new Date().getTime();
-        formBlogPostNew['id'] = timestamp;
+        if(!this.state.isUpdate) {
+            formBlogPostNew['id'] = timestamp;
+        }
         formBlogPostNew[event.target.name] = event.target.value;
         this.setState({
             formBlogPost: formBlogPostNew
@@ -54,7 +91,11 @@ class BlogPost extends Component {
     }
 
     handleSubmit = () => {
-        this.postDataToAPI(); // memanggil method postDataToAPI
+        if(this.state.isUpdate) {
+            this.putDataToAPI(); // memanggil method putDataToAPI
+        } else {
+            this.postDataToAPI(); // memanggil method postDataToAPI
+        }
     }
 
     componentDidMount() {
@@ -77,15 +118,15 @@ class BlogPost extends Component {
                 <p className="section-title">Blog Post</p>
                 <div className="form-add-post">
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" placeholder="add title" onChange={this.handleFormChange}/>
+                    <input type="text" value={this.state.formBlogPost.title} name="title" placeholder="add title" onChange={this.handleFormChange}/>
                     <label htmlFor="body">Blog Content</label>
-                    <textarea name="body" id="body" cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+                    <textarea name="body" id="body" cols="30" rows="10" value={this.state.formBlogPost.body} placeholder="add blog content" onChange={this.handleFormChange}></textarea>
                     <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
                 </div>
                 {
                     // looping data dari API
                     this.state.post.map(post => {
-                        return <Post key={post.id} data={post} remove={this.handleRemove} />
+                        return <Post key={post.id} data={post} remove={this.handleRemove} update={this.handleUpdate}/>
                     })
                 }
             </Fragment>
