7452d3ddfbf80fd58cfd5723742740c588274056
Supardi <supardi9512@gmail.com>
2019-08-07 22:19:04 +0700

    move-file-and-rename-router

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,9 +1,14 @@
+// libraries
 import React, {Component, Fragment} from 'react';
-import YouTubeComp from '../../component/YoutubeComp/YouTubeComp';
-import Product from '../Product/Product';
-import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
-import BlogPost from '../BlogPost/BlogPost';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+// pages
+import YoutubeCompPage from '../pages/YoutubeCompPage/YoutubeCompPage';
+import Product from '../pages/Product/Product';
+import LifeCycleComp from '../pages/LifeCycleComp/LifeCycleComp';
+import BlogPost from '../pages/BlogPost/BlogPost';
+
+// style
 import './Home.css';
 
 class Home extends Component {
@@ -24,58 +29,20 @@ class Home extends Component {
 
     render() {
         return (
-            <BrowserRouter>
-                {/* <div>
-                    <p>YouTube Component</p>
-                    <hr/>
-                    <YouTubeComp 
-                        time="7.12" 
-                        title="Tutorial React JS - Bagian 1"
-                        desc ="2x ditonton, 2 hari yang lalu"/>
-                    <YouTubeComp 
-                        time="8.02" 
-                        title="Tutorial React JS - Bagian 2"
-                        desc ="200x ditonton, 10 hari yang lalu"/>
-                    <YouTubeComp 
-                        time="5.04" 
-                        title="Tutorial React JS - Bagian 3"
-                        desc ="500x ditonton, 4 hari yang lalu"/>
-                    <YouTubeComp 
-                        time="4.12" 
-                        title="Tutorial React JS - Bagian 4"
-                        desc ="1k ditonton, 14 hari yang lalu"/>
-                    <YouTubeComp />
-
-
-
-                    <p>Counter</p>
-                    <hr/>
-                    <Product />
-
-
-
-                    <p>LifeCycle Component</p>
-                    <hr/>
-                    {
-                        // jika showComponent=true , munculkan komponennya, namun jika false tidak munculkan apapun
-                        this.state.showComponent ? <LifeCycleComp /> : null
-                    }
-
-                    <p>Blog Post</p>
-                    <hr/>
-                    <BlogPost />
-                </div> */}
+            <Router>
                 <Fragment>
                     <div className="navigation">
                         <Link to="/">Blog Post</Link>
                         <Link to="/product/">Product</Link>
                         <Link to="/lifecycle">LifeCycle</Link>
+                        <Link to="/youtube-component">Youtube</Link>
                     </div>
                     <Route path="/" exact component={BlogPost} />
                     <Route path="/product/" component={Product} />
                     <Route path="/lifecycle/" component={LifeCycleComp} />
+                    <Route path="/youtube-component/" component={YoutubeCompPage} />
                 </Fragment>
-            </BrowserRouter>
+            </Router>
             
         )
     }
